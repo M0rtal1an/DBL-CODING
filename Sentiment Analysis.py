e7871754837f042d26661a1468afbcfd9138a1c9
Chang Xu <c.xu1@student.tue.nl>
2023-06-02 14:55:38 +0200

    Vader scores interpretation (positive/negative/neutral)

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -3,17 +3,20 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 import sqlite3
 
 # Connect to the SQLite database
-connection = sqlite3.connect(r'C:\Users\20221859\OneDrive - TU Eindhoven\Documents\airlines.db')
+connection = sqlite3.connect(r'C:\Users\20221859\Downloads\airlines.db')
 
 # Retrieve the tweet data using a SQL query
 query = "SELECT id, text FROM data_new"
-alter_query = "ALTER TABLE data_new ADD sentiment_score FLOAT"
+alter_query = "ALTER TABLE data_new ADD COLUMN sentiment_score FLOAT"  # Add a new column for sentiment score
+alter_query2 = "ALTER TABLE data_new ADD COLUMN sentiment_label TEXT"  # Add a new column for sentiment label
 cursor = connection.cursor()
 cursor.execute("PRAGMA table_info(data_new)")
 columns = cursor.fetchall()
 column_names = [column[1] for column in columns]
 if 'sentiment_score' not in column_names:
     cursor.execute(alter_query)
+if 'sentiment_label' not in column_names:
+    cursor.execute(alter_query2)
 cursor.execute(query)
 tweets = cursor.fetchall()
 
@@ -32,6 +35,14 @@ for tweet in tweets:
     # Perform sentiment analysis on the tweet
     sentiment_scores = sia.polarity_scores(tweet_text)
     compound_score = sentiment_scores['compound']
+
+    # Assign a sentiment label based on the compound score
+    if compound_score >= 0.05:
+        sentiment_label = 'Positive'
+    elif compound_score <= -0.05:
+        sentiment_label = 'Negative'
+    else:
+        sentiment_label = 'Neutral'
 
     # Update the database with the sentiment score
     update_query = f"UPDATE data_new SET sentiment_score = {compound_score} WHERE id = {tweet_id}"
